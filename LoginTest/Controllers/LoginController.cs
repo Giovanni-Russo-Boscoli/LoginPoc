b834f202177d8997e6e46b24c9020d863497e35a
GIOVANNI RUSSO BOSCOLI <boscoli.giovanni@outlook.com>
2020-01-22 22:22:45 +0000

    Add project files.

---
+++ LoginTest/Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using LoginTest.Models;
+using System.DirectoryServices.AccountManagement;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LoginTest.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+
+            if (HttpContext.User.Identity.IsAuthenticated){
+                return RedirectToAction("Index", "Home");
+            }
+            
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignIn(LoginCustom model, string returnUrl)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            if (ModelState.IsValid)
+            {
+                //var userExists = loginApp.ValidLogin(model.UserName, model.Password, out int userId);
+                var userExists = IsAuthenticated(model.UserName, model.Password);
+
+                if (userExists)
+                {
+                    //Session["userId"] = userId;
+
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "The username or password is incorrect");
+            return View("Index", model);
+        }
+
+        public bool IsAuthenticated(string userName, string pwd)
+        {
+            //var domainName = "DESKTOP-A7IHSEN";
+            //var domainName = "welfare.irlgov.ie";
+            var isValid = false;
+
+            //using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domainName))
+            using (PrincipalContext pc = new PrincipalContext(ContextType.Machine))
+            {
+                isValid = pc.ValidateCredentials(userName, pwd);
+            }
+
+            return isValid;
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Index();
+        }
+    }
+}
---
+++ LoginTest/Models/LoginCustom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginTest.Models
+{
+    public class LoginCustom
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(20, ErrorMessage = "{0} must be 20 characters or less")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        public bool RememberMe { get; set; }
+    }
+}
